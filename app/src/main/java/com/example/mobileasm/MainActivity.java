1341bf5b981146260013cf5eb971124a8b29b540
Nguyễn Trọng Đạt <103309042+datnt2002@users.noreply.github.com>
2023-10-25 14:10:40 +0700

    update bottom navigation

--- app/src/main/java/com/example/mobileasm/MainActivity.java
+++ app/src/main/java/com/example/mobileasm/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.mobileasm;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
@@ -7,48 +8,58 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 
 import com.example.mobileasm.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
+    FloatingActionButton addHikeBtnFloatingMenu;
 
-    private Toolbar toolbar;
+    BottomNavigationView nav;
 
-    @SuppressLint("NonConstantResourceId")
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        setContentView(R.layout.activity_main);
 
-        replaceFragment(new HomeFragment());
-        binding.bottomNavigationView.setBackground(null);
-        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
-            int itemId = item.getItemId();
-
-            if (itemId == R.id.home) {
-                replaceFragment(new HomeFragment());
-            } else if (itemId == R.id.shorts) {
-                replaceFragment(new ShortFragment());
-            } else if (itemId == R.id.subscriptions) {
-                replaceFragment(new SubscriptionFragment());
-            } else if (itemId == R.id.library) {
-                replaceFragment(new LibraryFragment());
+        nav = findViewById(R.id.bottomNavigationView);
+        nav.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int itemId = item.getItemId();
+                Intent intent;
+                if (itemId == R.id.home) {
+                    intent = new Intent(MainActivity.this, SearchActivity.class);
+                    startActivity(intent);
+//                } else if (itemId == R.id.shorts) {
+//                    replaceFragment(new ShortFragment());
+//                } else if (itemId == R.id.subscriptions) {
+//                    replaceFragment(new SubscriptionFragment());
+//                } else if (itemId == R.id.library) {
+//                    replaceFragment(new LibraryFragment());
+//                }
+                }
+                return true;
             }
-            return true;
         });
 
-        toolbar = findViewById(R.id.my_app_bar);
-        setSupportActionBar(toolbar);
-    }
+        addHikeBtnFloatingMenu = findViewById(R.id.add_hike_btn_floating);
+        addHikeBtnFloatingMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AddActivity.class);
+                startActivity(intent);
+            }
+        });
 
-    private void replaceFragment(Fragment fragment){
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.frame_layout, fragment);
-        fragmentTransaction.commit();
     }
 }
