11fc71d022ef552110148fdde303b17e5b87e8a9
Nguyễn Trọng Đạt <103309042+datnt2002@users.noreply.github.com>
2023-10-28 21:17:53 +0700

    finish

--- app/src/main/java/com/example/mobileasm/NavigatorHandler.java
+++ app/src/main/java/com/example/mobileasm/NavigatorHandler.java
@@ -3,22 +3,15 @@ package com.example.mobileasm;
 import android.app.Activity;
 import android.content.Intent;
 
-import com.example.mobileasm.hikeActivity.SearchActivity;
-
 public class NavigatorHandler {
     public void navigateTo(int itemId, Activity currentActivity) {
         Intent intent;
         if (itemId == R.id.home) {
             intent = new Intent(currentActivity, MainActivity.class);
             currentActivity.startActivity(intent);
-        } else if (itemId == R.id.search) {
-            intent = new Intent(currentActivity, SearchActivity.class);
-            currentActivity.startActivity(intent);
+        } else if (itemId == R.id.profile) {
+//            intent = new Intent(currentActivity, SearchActivity.class);
+//            currentActivity.startActivity(intent);
         }
-        // Add other conditions as needed for other menu items
-
-
     }
-
-
 }
